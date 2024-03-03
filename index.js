f19e83bece6a48dbf9f596ca4b47d4de24e0c2e8
morizapta <162003363+morizapta@users.noreply.github.com>
2024-03-03 13:15:09 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,11 @@
 const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
 
+// Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
-const bot1 = new Eris(process.env.token1);
-const bot2 = new Eris(process.env.token2);
-const bot3 = new Eris(process.env.token3);
-const bot4 = new Eris(process.env.token4);
-const bot5 = new Eris(process.env.token5);
-const bot6 = new Eris(process.env.token6);
-const bot7 = new Eris(process.env.token7);
-const bot8 = new Eris(process.env.token8);
-const bot9 = new Eris(process.env.token9);
-const bot10 = new Eris(process.env.token10);
-const bot11 = new Eris(process.env.token11);
-const bot12 = new Eris(process.env.token12);
-const bot13 = new Eris(process.env.token13);
-const bot14 = new Eris(process.env.token14);
-const bot15 = new Eris(process.env.token15);
-const bot16 = new Eris(process.env.token16);
-const bot17 = new Eris(process.env.token17);
-const bot18 = new Eris(process.env.token18);
-const bot19 = new Eris(process.env.token19);
-const bot20 = new Eris(process.env.token20);
-const bot21 = new Eris(process.env.token21);
-const bot22 = new Eris(process.env.token22);
-const bot23 = new Eris(process.env.token23);
-const bot24 = new Eris(process.env.token24);
-const bot25 = new Eris(process.env.token25);
 
 bot.on("error", (err) => {
-  console.error(err);
+  console.error(err); // or your preferred logger
 });
 
-bot.connect();
-bot1.connect();
-bot2.connect();
-bot3.connect();
-bot4.connect();
-bot5.connect();
-bot6.connect();
-bot7.connect();
-bot8.connect();
-bot9.connect();
-bot10.connect();
-bot11.connect();
-bot12.connect();
-bot13.connect();
-bot14.connect();
-bot15.connect();
-bot16.connect();
-bot17.connect();
-bot18.connect();
-bot19.connect();
-bot20.connect();
-bot21.connect();
-bot22.connect();
-bot23.connect();
-bot24.connect();
-bot25.connect();
+bot.connect(); // Get the bot to connect to Discord
