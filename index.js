9c7a03c59c9526048ac8a4d089677d24471998d1
morizapta <162003363+morizapta@users.noreply.github.com>
2024-03-03 13:33:46 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
 
 // Replace TOKEN with your bot account's token
-const bot = new Eris(process.env.token1);
-const bot1 = new Eris(process.env.token2);
+const bot1 = new Eris(process.env.token1);
+const bot2 = new Eris(process.env.token2);
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
