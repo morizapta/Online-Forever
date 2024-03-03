28bdb4e387676f94519da2c7ebfe9a45f8900112
morizapta <162003363+morizapta@users.noreply.github.com>
2024-03-03 13:36:44 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
 
 // Replace TOKEN with your bot account's token
-const bot1 = new Eris(process.env.token1);
+const bot = new Eris(process.env.token);
 const bot2 = new Eris(process.env.token2);
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
 });
 
-bot1.connect(); // Get the bot to connect to Discord
+bot.connect(); // Get the bot to connect to Discord
 bot2.connect();
