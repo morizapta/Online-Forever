f6191dffe8f23b37715c04150cd2ca8c626b1961
morizapta <162003363+morizapta@users.noreply.github.com>
2024-09-09 03:09:27 +0600

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bot = new Eris(process.env.token);
 const bot2 = new Eris(process.env.token2);
 const bot3 = new Eris(process.env.token3);
 const bot4 = new Eris(process.env.token4);
+const bot4 = new Eris(process.env.token5);
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
@@ -15,3 +16,4 @@ bot.connect(); // Get the bot to connect to Discord
 bot2.connect();
 bot3.connect();
 bot4.connect();
+bot5.connect();
